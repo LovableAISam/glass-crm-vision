caaa31bd93db836b8327b101d6ed862bb98bdeda
Rizky Sam <rizkysamuelmp@gmail.com>
2024-11-13 17:28:08 +0700

    MTRBT-90

--- apps/co-dashboard/src/packages/kyc-request/components/ViewKYCRequestHistory/content/PersonalDataHistory.tsx
+++ apps/co-dashboard/src/packages/kyc-request/components/ViewKYCRequestHistory/content/PersonalDataHistory.tsx
@@ -218,7 +218,7 @@ function PersonalDataHistory(props: ViewKYCRequestTabProps) {
               {tKYC('personalDataZipCode')}
             </Typography>
             <Typography variant="subtitle2">
-              {kycDetail?.memberResidence.address || '-'}
+              {kycDetail?.memberResidence.postalCode || '-'}
             </Typography>
             <Divider />
           </Stack>
